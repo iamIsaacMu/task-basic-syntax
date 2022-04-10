0178bb26dc8441ff6709cb51ee75ead3994ccf02
iamIsaacMu <iamisaacmu@gmail.com>
2022-04-10 21:57:06 +0700

    adds

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,15 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    var obj = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    var digits = Object.keys(obj);
+
+    for (var i = 0; i < str.length; ++i) {
+        if (digits.indexOf(str[i]) < digits.indexOf(str[i + 1])) {
+            result = result - obj[str[i]];
+        } else {
+            result = result + obj[str[i]];
+        }
+    }
+
     return result;
 }
